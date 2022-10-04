3b4a4e02d81810544fd8329fb9f3732c214f3b55
shubhamkhandelwal <shubham.khandelwal019@gmail.com>
2022-10-04 20:55:14 +0200

    fix(API): Fix App Startup
    Run script using sudo
    Rollback Default client
    Run app on 80 in prod

--- expense-api/src/main/java/com/expense/expensemanagement/config/CognitoConfiguration.java
+++ expense-api/src/main/java/com/expense/expensemanagement/config/CognitoConfiguration.java
@@ -1,14 +1,20 @@
 package com.expense.expensemanagement.config;
 
-import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
-import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.expense.expensemanagement.service.cognito.CognitoService;
 import com.expense.expensemanagement.service.cognito.CognitoServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
+import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
 
 @Component
 public class CognitoConfiguration {
@@ -57,10 +63,11 @@ public class CognitoConfiguration {
 	@Bean("CognitoIdentityProvider")
 	public AWSCognitoIdentityProvider cognitoIdentityProvider() throws Exception {
 		logger.debug("Configuring Cognito");
-//		AWSCredentials cred = new Defa
-//		AWSCredentialsProvider credProvider = new AWSStaticCredentialsProvider(cred);
-//
-		AWSCognitoIdentityProvider cognitoIdentityProvider = AWSCognitoIdentityProviderClientBuilder.defaultClient();
+		AWSCredentials cred = new BasicAWSCredentials(this.accessKey, this.secretKey);
+		AWSCredentialsProvider credProvider = new AWSStaticCredentialsProvider(cred);
+		AWSCognitoIdentityProvider cognitoIdentityProvider = AWSCognitoIdentityProviderClientBuilder.standard()
+				.withRegion(this.region)
+				.withCredentials(credProvider).build();
 		logger.debug("Cognito initialized successfully");
 		return cognitoIdentityProvider;
 	}
